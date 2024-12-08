eaccf36ae9f400ed90ab958efab30e8d73f8df0a
badlyboundbible <dmillar1919@gmail.com>
2024-12-08 07:34:06 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-function calculateScoresAndPublish() {
-  const jackScores = [...document.querySelectorAll(".jack input[type='number']:last-child")].map(
-    input => parseInt(input.value) || 0
-  );
-  const ellScores = [...document.querySelectorAll(".ell input[type='number']:last-child")].map(
-    input => parseInt(input.value) || 0
-  );
+document.getElementById("publish").addEventListener("click", () => {
+  const jackScores = [...document.querySelectorAll(".jack .player-card input:last-of-type")].map(input => Number(input.value) || 0);
+  const ellScores = [...document.querySelectorAll(".ell .player-card input:last-of-type")].map(input => Number(input.value) || 0);
 
   const jackTotal = jackScores.reduce((a, b) => a + b, 0);
   const ellTotal = ellScores.reduce((a, b) => a + b, 0);
@@ -15,15 +11,13 @@ function calculateScoresAndPublish() {
   const winner = jackTotal > ellTotal ? "Jack" : ellTotal > jackTotal ? "Ell" : "Draw";
   document.getElementById("winner").textContent = `Winner: ${winner}`;
 
-  const resultsTable = document.querySelector(".score-table tbody");
+  const table = document.querySelector(".score-table tbody");
   const newRow = document.createElement("tr");
   newRow.innerHTML = `
-    <td>Week ${resultsTable.children.length + 1}</td>
+    <td>Week ${table.children.length + 1}</td>
     <td>${jackTotal}</td>
     <td>${winner}</td>
     <td>${ellTotal}</td>
   `;
-  resultsTable.appendChild(newRow);
-}
-
-document.getElementById("publish").addEventListener("click", calculateScoresAndPublish);
+  table.appendChild(newRow);
+});
