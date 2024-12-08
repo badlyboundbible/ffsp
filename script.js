d4d617d4fe5d2028df6d72ae5d5b6720a1c2be1e
badlyboundbible <dmillar1919@gmail.com>
2024-12-08 12:11:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// Import the Supabase library (use type="module" in index.html)
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 // Initialize Supabase client
 const supabase = createClient(
     "https://cfmtrhlovulezzswxqsu.supabase.co", // Supabase URL
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmbXRyaGxvdnVsZXp6c3d4cXN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM2NDc1NjEsImV4cCI6MjA0OTIyMzU2MX0.J72n-YGyt1HkeYG4GGuKvZ9JeSZDz4rj1pI6bYPLEIU" // Supabase Public API Key
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmbXRyaGxvdnVsZXp6c3d4cXN1Iiwicm9zZSI6ImFub24iLCJpYXQiOjE3MzM2NDc1NjEsImV4cCI6MjA0OTIyMzU2MX0.J72n-YGyt1pHkeYG4GGuKvZ9JeSZDz4rj1pI6bYPLEIU" // Supabase Public API Key
 );
 
 // Test Supabase connection
